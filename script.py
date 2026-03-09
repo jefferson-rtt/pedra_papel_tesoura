0444f6b5cfb64738437689a439a371ffe96f0357
jefferson-rtt <jefferson.rtt@outlook.com>
2026-03-09 20:37:50 -0300

    Add script.py

---
+++ script.py
@@ -0,0 +1,53 @@
+print("-" * 50)
+print("Jogo - Pedra, Papel e Tesoura")
+print("-" * 50)
+
+'''
+Regras do jogo:
+
+- Pedra vence Tesoura
+- Tesoura vence Papel
+- Papel vence Pedra
+
+'''
+
+#opções de jogada
+opcoes_validas = ("pedra", "papel", "tesoura")
+print(f"Opções de jogada: {opcoes_validas}")
+
+print()
+
+
+#entrada do usuário
+j1 = input("Jogador 1 - Escolha sua mão: ").lower().strip()
+if j1 not in opcoes_validas:
+    print("Jogada inválida! Tente novamente.")
+    exit()
+j2 = input("Jogador 2 - Escolha a mão: ").lower().strip()
+if j2 not in opcoes_validas:
+    print("Jogada inválida! Tente novamente.")
+    exit()
+
+print("-" * 50)
+print()
+print(f"Escolha do jogador 1: {j1}")
+print(f"Escolha do jogador 2: {j2}")
+print()
+
+print("-" * 50)
+
+#verificacao de vecendor
+
+if j1 == j2:
+    print("Empate!")
+elif (j1 == "Pedra" and j2 == "Tesoura")or\
+    (j1 == "Tesoura" and j2 == "Papel")or\
+    (j1 == "Papel" and j2 == "Pedra"):
+    print("Jogador 1 VENCEU, Parabéns!")
+
+else:
+    print()
+    print("Jogador 2 VENCEU, Parabéns!")
+
+print()
+print("-" * 50)
